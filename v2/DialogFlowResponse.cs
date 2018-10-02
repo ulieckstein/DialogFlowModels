9695ae52dc87143e8bb6e38425d503ac075ab9e3
Unknown <ulieckstein@gmail.com>
2018-10-02 15:37:24 +0200

    fixed model

--- v2/DialogFlowResponse.cs
+++ v2/DialogFlowResponse.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using DialogFlowModels.v1.ResponseTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DialogFlowModels.v2
 {
@@ -20,18 +24,33 @@ namespace DialogFlowModels.v2
 
         public void AddTextResponse(string text)
         {
+            FulfillmentText = text;
             FulfillmentMessages.Clear();
             FulfillmentMessages.Add(new FulfillmentMessage(text));
 
             var payload = new ResponsePayload
             {
                 ExpectUserResponse = false,
-                Items = new List<AbstractRichResponse>
-                {
-                    new Text(text, text)
+                RichResponse = new RichResponsePayload { 
+                    Items = new List<AbstractRichResponse>
+                    {
+                        new Text(text, text)
+                    }
                 }
             };
             Payload["google"] = payload;
+        }
+
+        public HttpResponseMessage GenerateResponseMessage()
+        {
+            var serializerSettings = new JsonSerializerSettings();
+            serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            
+            var resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(this, serializerSettings), System.Text.Encoding.UTF8, "text/plain")
+            };
+            return resp;
         }
     }
 
@@ -58,6 +77,21 @@ namespace DialogFlowModels.v2
     public class ResponsePayload
     {
         public bool ExpectUserResponse { get; set; }
+        public RichResponsePayload RichResponse { get; set; }
+
+        public ResponsePayload()
+        {
+            RichResponse = new RichResponsePayload();
+        }
+    }
+
+    public class RichResponsePayload
+    {
         public List<AbstractRichResponse> Items { get; set; }
+
+        public RichResponsePayload()
+        {
+            Items = new List<AbstractRichResponse>();
+        }
     }
 }
