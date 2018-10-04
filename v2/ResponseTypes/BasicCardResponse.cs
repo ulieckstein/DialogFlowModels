464b26eeacb7e9ea9c931aef5d17f380feebc93d
Unknown <ulieckstein@gmail.com>
2018-10-04 14:21:44 +0200

    added basic card helper functions

--- v2/ResponseTypes/BasicCardResponse.cs
+++ v2/ResponseTypes/BasicCardResponse.cs
@@ -7,6 +7,75 @@ namespace DialogFlowModels.v2.ResponseTypes
     public class BasicCardResponse
     {
         public BasicCard BasicCard { get; set; }
+
+        public BasicCardResponse(string title, string subtitle)
+        {
+            BasicCard = new BasicCard {Title = title, Subtitle = subtitle};
+        }
+
+        public BasicCardResponse(string title, string subtitle, string formattedText)
+        {
+            BasicCard = new BasicCard { Title = title, Subtitle = subtitle, FormattedText = formattedText};
+        }
+
+        public BasicCardResponse(string title, string subtitle, string imageUrl, string accessibilityText)
+        {
+            BasicCard = new BasicCard
+            {
+                Title = title,
+                Subtitle = subtitle,
+                Image = new Image {AccessibilityText = accessibilityText, Url = imageUrl}
+            };
+        }
+
+        public BasicCardResponse AddImage(string imageUrl, string accessibilityText)
+        {
+            BasicCard.Image = new Image {AccessibilityText = accessibilityText, Url = imageUrl};
+            return this;
+        }
+
+        public BasicCardResponse AddImage(string imageUrl, string accessibilityText, int width, int height)
+        {
+            BasicCard.Image = new Image { AccessibilityText = accessibilityText, Url = imageUrl, Width = width, Height = height};
+            return this;
+        }
+
+        public BasicCardResponse SetImageDisplayOptions(ImageDisplayOptions options)
+        {
+            BasicCard.ImageDisplayOptions = options;
+            return this;
+        }
+
+        public BasicCardResponse SetImageDimensions(int width, int height)
+        {
+            BasicCard.Image.Width = width;
+            BasicCard.Image.Height = height;
+            return this;
+        }
+
+        public BasicCardResponse AddButton(string title, string url)
+        {
+            BasicCard.Buttons.Add(new Button{Title = title, OpenUrlAction = new OpenUrlAction{Url = url}});
+            return this;
+        }
+
+        public BasicCardResponse AddButton(string title, string url, string androidPackageName)
+        {
+            BasicCard.Buttons.Add(
+                new Button
+                {
+                    Title = title,
+                    OpenUrlAction = new OpenUrlAction
+                    {
+                        Url = url,
+                        AndroidApp = new AndroidApp
+                        {
+                            PackageName = androidPackageName
+                        }
+                    },
+                });
+            return this;
+        }
     }
 
     public class BasicCard
@@ -17,6 +86,11 @@ namespace DialogFlowModels.v2.ResponseTypes
         public Image Image { get; set; }
         public List<Button> Buttons { get; set; }
         public ImageDisplayOptions ImageDisplayOptions { get; set; }
+
+        public BasicCard()
+        {
+            Buttons = new List<Button>();
+        }
     }
 
     public class Image
