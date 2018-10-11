2ca77df9a1f1c07912f7bf22449790a31c1f6a23
Unknown <ulieckstein@gmail.com>
2018-10-11 12:28:10 +0200

    created carouselBrowse classes

---
+++ v2/ResponseTypes/CarouselBrowseResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogFlowModels.v2.ResponseTypes
+{
+    public class CarouselBrowseResponse
+    {
+        public CarouselBrowse CarouselBrowse { get; set; }
+
+    }
+
+    public class CarouselBrowse
+    {
+        public List<CarouselBrowseItem> Items { get; set; }
+        public ImageDisplayOptions ImageDisplayOptions { get; set; }
+    }
+
+    public class CarouselBrowseItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Footer { get; set; }
+        public Image Image { get; set; }
+        public OpenUrlAction OpenUrlAction { get; set; }
+    }
+}
