08e9f74fc6c0424f0a4d22a156b8b5020b852adb
Unknown <ulieckstein@gmail.com>
2018-10-11 12:47:09 +0200

    created constuctor functions for carouselBrowseResponse

--- v2/ResponseTypes/CarouselBrowseResponse.cs
+++ v2/ResponseTypes/CarouselBrowseResponse.cs
@@ -8,12 +8,60 @@ namespace DialogFlowModels.v2.ResponseTypes
     {
         public CarouselBrowse CarouselBrowse { get; set; }
 
+        public CarouselBrowseResponse()
+        {
+            CarouselBrowse = new CarouselBrowse();
+        }
+
+        public CarouselBrowseResponse AddItem(string title, string targetUrl)
+        {
+            CarouselBrowse.Items.Add(new CarouselBrowseItem{Title = title, OpenUrlAction = new OpenUrlAction{Url = targetUrl}});
+            return this;
+        }
+
+        public CarouselBrowseResponse AddItem(string title, string description, string footer, string targetUrl)
+        {
+            CarouselBrowse.Items.Add(
+                new CarouselBrowseItem
+                {
+                    Title = title,
+                    Description = description,
+                    Footer = footer,
+                    OpenUrlAction = new OpenUrlAction { Url = targetUrl }
+                });
+            return this;
+        }
+
+        public CarouselBrowseResponse AddItem(string title, string description, string footer, string imageUrl, string accessibilityText, string targetUrl)
+        {
+            CarouselBrowse.Items.Add(
+                new CarouselBrowseItem
+                {
+                    Title = title,
+                    Description = description,
+                    Footer = footer,
+                    Image = new Image { Url = imageUrl, AccessibilityText = accessibilityText},
+                    OpenUrlAction = new OpenUrlAction { Url = targetUrl }
+                });
+            return this;
+        }
+
+        public CarouselBrowseResponse AddItem(CarouselBrowseItem item)
+        {
+            CarouselBrowse.Items.Add(item);
+            return this;
+        }
     }
 
     public class CarouselBrowse
     {
         public List<CarouselBrowseItem> Items { get; set; }
         public ImageDisplayOptions ImageDisplayOptions { get; set; }
+
+        public CarouselBrowse()
+        {
+            Items = new List<CarouselBrowseItem>();
+        }
     }
 
     public class CarouselBrowseItem
